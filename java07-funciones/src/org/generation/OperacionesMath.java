bed70328517cdf314e0c69407edf52d14bfa4c26
Sergio Torres <sergio.torres@idr.edu.mx>
2023-08-30 11:17:26 -0600

    ci: clase OperacionesMath

---
+++ java07-funciones/src/org/generation/OperacionesMath.java
@@ -0,0 +1,24 @@
+package org.generation;
+
+public class OperacionesMath {
+	
+	// atributos de instancia ( non-static fields)
+	// atributos de clase (static fields)
+	
+	
+	// métodos de instancia	
+	
+	// métodos de clase (static methods)
+	/* Los métodos estáticos se relacia la clase en lugar de las
+	 * instancias.
+	 * Se puede invocar sin tener que instanciar un objeto.
+	 * 
+	 */
+	static double valorPi(){
+		double pi = Math.PI;
+		return pi;
+	}
+	
+	
+
+}
---
+++ java07-funciones/src/org/generation/OperacionesMathTest.java
@@ -0,0 +1,13 @@
+package org.generation;
+
+public class OperacionesMathTest {
+
+	public static void main(String[] args) {
+		
+		double valorPiActual = OperacionesMath.valorPi();
+		System.out.println(valorPiActual);
+		
+
+	}
+
+}
