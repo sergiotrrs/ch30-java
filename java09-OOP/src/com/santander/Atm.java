c68e5bb3c178a296843d592fcca42872ade0ff61
Sergio Torres <sergio.torres@idr.edu.mx>
2023-09-04 12:20:36 -0600

    feat: clase ATM

---
+++ java09-OOP/src/com/santander/Atm.java
@@ -0,0 +1,53 @@
+package com.santander;
+
+public class Atm {
+	
+	// atributos de instancia (non-static fileds)
+	double balance;
+	
+	// atributos de clase (static fields)
+		
+	/**
+	 * Método constructor
+	 * 
+	 * El método constructor se utiliza para iniciar objetos de una clase
+	 * El constructor se llama automáticamente cuando se crea una instancia
+	 * de la clase utilizando la pabra reservada new.
+	 * Su propósito es asignar valores iniciales a los objetos
+	 * Características:
+	 *  - Su nombre es idéntico al de la clase.
+	 *  - No tiene un tipo de retorno explícito.
+	 *  - Puede tener parámetros
+	 *  - Puede ser sobrecargado
+	 */
+	 Atm(){
+		// Constructor por default: no tiene parámetros
+		// Si no se agrega algún costructor, java proporciona este método
+		 System.out.println("Me estoy instanciando");
+	}
+	 /**
+	  * Método sobrecargado.
+	  * Los métodos sobrecargados tienen el mismo nombre
+	  * pero diferentes parámetros
+	  * @param blance cantidad de dinero disponible en el cajero
+	  * 
+	  * this es una palabra reservada que se utiliza para hacer
+	  * referencia al objeto en el que se encuentra el contexto
+	  * de la ejecución.
+	  * this se utiliza para referirse al objeto actual,
+	  */
+	Atm(double balance){	 	
+		// para invocar a un costructor, se usa this y (parámetros)
+		// el llamado a otro constructor debe ser la primera línea
+		this();
+		// balance = balanceIn;
+		this.balance = balance;
+		System.out.println("Tengo la cantidad de " + this.balance);
+	}
+	// métodos de instancia
+	
+	// métodos de clase
+	
+	
+
+}
---
+++ java09-OOP/src/com/santander/AtmTest.java
@@ -0,0 +1,13 @@
+package com.santander;
+
+public class AtmTest {
+
+	public static void main(String[] args) {
+
+		Atm cajeroA = new Atm();
+		Atm cajeroB = new Atm(10_000);
+		Atm cajeroC = new Atm(30_000);
+		
+	}
+
+}
