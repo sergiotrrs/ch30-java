1ee91c1ae847587b0c310d2b4279ec9df213e24f
Sergio Torres <sergio.torres@idr.edu.mx>
2023-09-05 14:26:37 -0600

    ci: ejercicio atm

---
+++ java10-encapsulacion/src/com/citibanamex/Atm.java
@@ -0,0 +1,29 @@
+package com.citibanamex;
+
+public class Atm {
+	/*
+	 *  Crear una clase de cajero automático que contenga:
+	 *   -  2 atributos de instancia
+	 *      > double balance
+	 *      > int serialNumber (cajero)
+	 *   - 1 contructor
+	 *   - encapsular los atributos
+	 *   - generar los setters y getters
+	 *   
+	 *   - método que acepte depósitos y lo suma al balance
+	 *     deposit()
+	 *       > la cantida es negativa, lanzar una excepción
+	 *   - método que se hagan retiros y resta del balance
+	 *     withdraw()
+	 *       > si la cantidad a retirar es superior al balance
+	 *        lanzar una excepción.
+	 *   - Realizar las pruebas del objeto Atm en otra clase de
+	 *     otro paquete.     
+	 *       
+	 *   Opcional:
+	 *   - Agregar un log de los depósitos y retiros
+	 *     >  atributo private ArrayList<String> log = new ArrayList<>();
+		   > HashMap<String, Participante > log = new HashMap<>();
+	 */
+
+}
