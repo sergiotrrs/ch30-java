7595fb05ef341dccf5ae5e57edda7e1665867e2a
Sergio Torres <sergio.torres@idr.edu.mx>
2023-09-20 17:30:23 -0600

    feat: uso de JPA y Hibernate

--- sp2-injection/src/main/java/org/generation/app/controller/CustomerController.java
+++ sp2-injection/src/main/java/org/generation/app/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.generation.app.entity.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -38,5 +39,25 @@ public class CustomerController {
 		Map<Integer, Customer> customers = Customer.usersMock();
 		return customers.get(idCustomer);
 	}
+	
+	/*
+	 *  Request Param
+	 *  Vincula los parámetros de una solicitud HTTP con los
+	 *  parámetros del método.
+	 *  Permite extraer los valores de los marámetros de 
+	 *  la consulta (query parameters).
+	 *  Si no se proporciopna los valores, se genera la excepción
+	 *  MissingServletRequestParametersException
+	 *  
+	 */
+	@GetMapping("api/v1/customers/query") 
+	// localhost:8080/api/v1/customers/query?id=1&first_name=pato
+	public Customer customerRequestVariable(
+			@RequestParam(name="id") int idCustomer
+			) {
+		Map<Integer, Customer> customers = Customer.usersMock();
+		return customers.get(idCustomer);	
+	}
+	
 	
 }
